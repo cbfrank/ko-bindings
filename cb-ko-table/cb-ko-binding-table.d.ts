b2731b1596b77894ef278ca047b7ee51c34ae992
cb <cb@cb.com>
2014-08-11 14:08:23 +0800

    change namespace name
    add tableCreater var

--- cb-ko-table/cb-ko-binding-table.d.ts
+++ cb-ko-table/cb-ko-binding-table.d.ts
@@ -1,4 +1,4 @@
-﻿declare module $CB.ko.binding.table {
+﻿declare module $CB.Ko.Binding.Table {
     interface ICRUDActionTypes {
         add: string;
         'delete': string;
@@ -23,8 +23,43 @@
         field: string;
         asc: boolean; //true means asc, false means desc
     }
+
+    export class TableCreater {
+        templatesTagName: string;
+        cellTemplateAttrName: string;
+        cellTemplateTagName: string;
+        editorTemplateAttrName: string;
+        editorTemplateTagName: string;
+        dataItemPropertyAttrName: string;
+        tdClassAttrName: string;
+        notEditAttrName: string;
+        notAddAttrName: string;
+        notOrderAttName: string;
+        editorFinishEventAttrName: string;
+        editorFinishAttrName: string;
+        orderFieldAttrName: string;
+        selectedRowClass: string;
+        focusRowClass: string;
+        selectedCellClass: string;
+        focusCellClass: string;
+        inlineEditingCellClass: string;
+        orderedAscHeaderClass: string;
+        orderedDescHeaderClass: string;
+        orderableColumnClass: string;
+        buttonDefaultNames: {
+            addBtnName: string;
+            delBtnName: string;
+            changeBtnName: string;
+            saveBtnName: string;
+        };
+
+        crudButtonsContainerClass: string;
+        defaultModalEditorContainerBodyClass: string;
+    }
 }
 
+declare var tableCreater: $CB.Ko.Binding.Table.TableCreater;
+
 interface KnockoutBindingHandlers {
-    tableCRUD: $CB.ko.binding.table.ITableCRUDBindingHandler;
+    tableCRUD: $CB.Ko.Binding.Table.ITableCRUDBindingHandler;
 }
