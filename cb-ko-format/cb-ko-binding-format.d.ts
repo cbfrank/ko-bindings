0e81841e39013978cd54be436c85ffce4b28ebbf
cb <cb@cb.com>
2014-12-03 10:53:19 +0800

    add typescript def file

---
+++ cb-ko-format/cb-ko-binding-format.d.ts
@@ -0,0 +1,12 @@
+﻿declare module $CB.Ko.Binding.Format {
+    export interface IFormatBindingHandler extends KnockoutBindingHandler {
+        dataTypes: {
+            number: string;
+            dateTime: string;
+            func: string;
+        }
+    }
+}
+interface KnockoutBindingHandlers {
+    format: $CB.Ko.Binding.Format.IFormatBindingHandler;
+}
