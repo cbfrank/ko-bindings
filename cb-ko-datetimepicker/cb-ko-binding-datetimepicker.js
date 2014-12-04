329edb7450849f9bad7333eddbb0966a87125577
cb <cb@cb.com>
2014-12-04 15:46:12 +0800

    fix the bootstrap version check error, .container-fluid is supported in v3

--- cb-ko-datetimepicker/cb-ko-binding-datetimepicker.js
+++ cb-ko-datetimepicker/cb-ko-binding-datetimepicker.js
@@ -55,7 +55,7 @@ useage:
     };
 
     $(document).ready(function () {
-        if ($(document).find(".container-fluid, .row-fluid, .span1, .span2, .span3, .span4, .span5, .span6, .span7, .span8, .span9, .span10, .span11, .span12").length > 0) {
+        if ($(document).find(".row-fluid, .span1, .span2, .span3, .span4, .span5, .span6, .span7, .span8, .span9, .span10, .span11, .span12").length > 0) {
             ko.bindingHandlers.datetimepicker.bootstrapVersion = 2;
         }
     });
